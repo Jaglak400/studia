65096386f699ed606010d55c9cd70c6a2f99eb39
Jaglak400 <105947321+Jaglak400@users.noreply.github.com>
2022-10-28 22:24:03 +0200

    Add files via upload

---
+++ zad2 1-6/zad 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+class GFG
+{
+
+    public static void Main()
+    {
+        double x = Math.Pow(Math.Sqrt(2), Math.Sqrt(3));
+        double y = Math.Pow(Math.Sqrt(3), Math.Sqrt(2));
+        Console.WriteLine("Wynik: {0} i {1}", x ,y);
+        Console.WriteLine("Wieksza liczba to: " + Math.Max(x, y));
+
+    }
+}
---
+++ zad2 1-6/zad 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Year : ");
+
+            int Year = int.Parse(Console.ReadLine());
+
+            if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0)) 
+                Console.WriteLine("{0} is a Leap Year.", Year);
+
+            else Console.WriteLine("{0} is not a Leap Year.", Year);
+
+        }
+    }
